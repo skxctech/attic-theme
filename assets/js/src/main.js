c10f10d20866a3cc355a33b2f9e6176ff75f157c
Radu Simion <radu.simion@catalystww.com>
2013-10-22 10:06:36 +0300

    lots'o theme stuff

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,3 +1,12 @@
+'use strict';
+
 var testingGrunt = 'This is grunt testing';
 
 console.log(testingGrunt);
+
+$('#open-author-area').on('click', function() {
+	console.log('click');
+	$('body').toggleClass('menu-open');
+});
+
+$('code').addClass('prettyprint');
